817b9dfc60ffeb8f2dc6df9520a9630a9e71467a
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-08 16:30:25 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -17,6 +17,7 @@ class LoginForm extends React.Component {
   }
 
   handleFormSubmition = (event) => {
+    event.persist();
     event.preventDefault();
     if ( this.state.username && this.state.password ) {
       event.target.submit();
